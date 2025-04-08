2e24c2997916070d794c0b66836862d6d4522b70
Devin Reeks <devinreeks@192-168-1-119.tpgi.com.au>
2025-04-08 15:04:19 +1000

    🌷 UI(compiler): implemented designer styles

--- components/SectionContainer.tsx
+++ components/SectionContainer.tsx
@@ -12,7 +12,7 @@ export default function SectionContainer({ children }: Props) {
 
   return (
     <div
-      className={`mx-auto max-w-3xl px-4 sm:px-6 ${
+      className={`mx-auto max-w-3xl px-4 sm:p-6 ${
         !isRootRoute ? 'xl:max-w-5xl xl:px-0' : ''
       }`}
     >
